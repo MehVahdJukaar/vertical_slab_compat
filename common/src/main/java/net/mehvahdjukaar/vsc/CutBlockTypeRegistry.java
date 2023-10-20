10760afc25e91ca9f90f5b39395eebdccee037f0
MehVahdJukaar <matteogozzi1998@gmail.com>
2023-10-20 22:50:03 +0300

    fix your ids absent by design or i make a new mod that replaces you >:(
    (cherry picked from commit 50fe0f591cbe6976db27393f4637715852e82f7c)

--- common/src/main/java/net/mehvahdjukaar/vsc/CutBlockTypeRegistry.java
+++ common/src/main/java/net/mehvahdjukaar/vsc/CutBlockTypeRegistry.java
@@ -6,12 +6,13 @@ import net.mehvahdjukaar.moonlight.api.events.AfterLanguageLoadEvent;
 import net.mehvahdjukaar.moonlight.api.set.BlockTypeRegistry;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.DyeColor;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SlabBlock;
 import org.jetbrains.annotations.Contract;
 
-import java.util.Optional;
+import java.util.*;
 
 public class CutBlockTypeRegistry extends BlockTypeRegistry<CutBlockType> {
     public static final CutBlockType STONE_TYPE = new CutBlockType(new ResourceLocation("stone"), Blocks.STONE, Blocks.STONE_SLAB);
@@ -25,6 +26,9 @@ public class CutBlockTypeRegistry extends BlockTypeRegistry<CutBlockType> {
         return STONE_TYPE;
     }
 
+    private final List<String> dyes = Arrays.stream(DyeColor.values()).map(DyeColor::getName)
+            .sorted(Comparator.comparingInt(String::length)).sorted(Collections.reverseOrder()).toList();
+
     @Override
     public Optional<CutBlockType> detectTypeFromBlock(Block block, ResourceLocation baseRes) {
         String name = null;
@@ -37,14 +41,27 @@ public class CutBlockTypeRegistry extends BlockTypeRegistry<CutBlockType> {
         String namespace = baseRes.getNamespace();
         if (name != null && block instanceof SlabBlock && !namespace.equals("securitycraft")) {
             ResourceLocation id = new ResourceLocation(namespace, name);
-            var o = Registry.BLOCK.getOptional(id);
-            if (o.isEmpty())
-                o = Registry.BLOCK.getOptional(new ResourceLocation(namespace, name + "s"));
-            if (o.isEmpty())
-                o = Registry.BLOCK.getOptional(new ResourceLocation(namespace, name + "_planks"));
-            if (o.isEmpty()) o = Registry.BLOCK.getOptional(new ResourceLocation(name));
-            if (o.isPresent() && hasRightShapeHack(block)) {
-                return Optional.of(new CutBlockType(id, o.get(), block));
+            var parent = Registry.BLOCK.getOptional(id);
+
+            if (parent.isEmpty() && namespace.equals("absentbydesign")) {
+                String finalName = name.replace("silver", "light_gray");
+                for (var d : dyes) {
+                    if (finalName.contains(d)) {
+                        var n = d + "_" + finalName.replace("_" + d, "");
+                        parent = Registry.BLOCK.getOptional(new ResourceLocation(n));
+                        if (parent.isPresent()) id = new ResourceLocation("absentbydesign", n);
+                    }
+                }
+                name = name.replace("bricks_cracked", "cracked_stone_bricks");
+
+            }
+            if (parent.isEmpty())
+                parent = Registry.BLOCK.getOptional(new ResourceLocation(namespace, name + "s"));
+            if (parent.isEmpty())
+                parent = Registry.BLOCK.getOptional(new ResourceLocation(namespace, name + "_planks"));
+            if (parent.isEmpty()) parent = Registry.BLOCK.getOptional(new ResourceLocation(name));
+            if (parent.isPresent() && hasRightShapeHack(block)) {
+                return Optional.of(new CutBlockType(id, parent.get(), block));
             }
         }
         return Optional.empty();
